0507c248fdb2f1d580669fc93a840dc3b0ea82e4
Abd2lrady <abd2lrady@gmail.com>
2022-11-06 10:12:21 +0200

    Deleting Main Storyboard

--- iOS_Rasid/AppDelegate + SceneDelegate/SceneDelegate + Extension.swift
+++ iOS_Rasid/AppDelegate + SceneDelegate/SceneDelegate + Extension.swift
@@ -5,8 +5,18 @@
 //  Created by Fintech on 06/11/2022.
 //
 
-import Foundation
+import UIKit
 
 extension SceneDelegate {
-    
+    func bootApp(scene: UIWindowScene) {
+        let window = UIWindow(windowScene: scene)
+        
+        let view = ViewController()
+        let navigator = UINavigationController(rootViewController: view)
+
+
+        window.rootViewController = navigator
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
 }
