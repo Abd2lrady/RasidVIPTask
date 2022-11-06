a22871d3706912011626519a267047d21dcc83cc
Abd2lrady <abd2lrady@gmail.com>
2022-11-06 10:14:46 +0200

    adding Constants Enum

---
+++ iOS_Rasid/Resources/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  iOS_Rasid
+//
+//  Created by Fintech on 06/11/2022.
+//
+
+import Foundation
+
+enum Constants {
+    enum API {
+        static let baseURL = ""
+    }
+}
